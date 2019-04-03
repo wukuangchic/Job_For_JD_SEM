101984ba0c01b77c54aa32789d8b9c06101dca90
wukuangchic <47804106+wukuangchic@users.noreply.github.com>
2019-04-03 10:54:26 +0800

    Update 查询京东是否有此商品.py

--- 查询京东是否有此商品.py
+++ 查询京东是否有此商品.py
@@ -10,13 +10,16 @@ def get_brand_searchurl(i):##品牌名转换为京东搜索结果页
     except:
         print("获取品牌错误")
 temp = input("本程序将查询京东搜索结果中是否有此商品\n请在同一目录创建“京东待查询关键词.csv”\n按回车键继续")
-file = open("京东待查询关键词.csv")
+file = open("京东待查询关键词"+time.strftime("%Y-%m-%d",time.localtime())+".csv")
+numi = 0
+file2 = open("导出-"+time.strftime("%Y-%m-%d",time.localtime())+".xls","a")
 for each in file.readlines():
     try:
         each = each.replace("\n","")
-        print(each)
-        list_temp = [each]
-        sop = BeautifulSoup(urlopen(get_brand_searchurl(each)).read(),"lxml")
+        print("%d-----%s"%(numi,each))
+        list_temp = []
+        list_temp1 = [each]
+        sop = BeautifulSoup(urlopen(get_brand_searchurl(each),timeout = 30).read(),"lxml")
         list1 = []
         try:
             for cherror in sop.findAll("div",{"class","check-error"}):
@@ -25,16 +28,38 @@ for each in file.readlines():
                 print(cherror)
             if list1 != []:
                 i = (cherror.replace("\n","")).replace(" ","")
+                i = i.replace("\t","")
             else:
                 i = "找到啦"
         except:
-            i = "找到啦"            
-        list_temp.append(i)
-        file2 = open("京东查询结果"+time.strftime("%Y-%m-%d",time.localtime())+".xls","a")
+            i = "sop出错"
+        list_temp2 = [i]
+        for i in sop.findAll("div",{"class","sl-key"}):
+            i = i.get_text()
+            i = i.replace("\n","")
+            i = i.replace("\t","")            
+            list_temp1.append(i)
+        for i in sop.findAll("div",{"class","sl-value"}):
+            i = i.get_text()
+            i = i.replace("\n","")
+            i = i.replace("\t","")
+            i = i.replace("确定取消","")
+            list_temp2.append(i)
+        print(list_temp1[2:])
+        for i in range(0,20):
+            try:                
+                list_temp.append(list_temp1[i])
+                list_temp.append(list_temp2[i])
+            except:
+                break
+        if numi % 97 == 0:
+            file2.close()
+            file2 = open("导出-"+time.strftime("%Y-%m-%d",time.localtime())+".xls","a")
         for i in list_temp:
             file2.write(i)
             file2.write("\t")
         file2.write("\n")
-        file2.close()
+        numi +=1
     except:
-        temp = 0
+        print("出错啦")
+file2.close()
